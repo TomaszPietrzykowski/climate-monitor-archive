5cfb1ddbfae94026c28cd23fb3c9f7628e0d9f9e
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-03-02 00:30:22 +0100

    context consumer added

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React from "react"
+import { ThemeContex } from "../../contexts/ThemeContext"
 
 const Navbar = () => {
-    return (
-        <div className='nav-bar'>
-            <h2 className="logo">Climate Monitor</h2>
-            <ul className='nav'>
-                <li>Home</li>
-                <li>Co2</li>
-                <li>Temperature</li>
-                <li>About</li>
+  return (
+    <ThemeContex.Consumer>
+      {context => {
+        const { isDarkMode, light, dark } = context
+        const theme = isDarkMode ? dark : light
+        return (
+          <nav
+            className="nav-bar"
+            style={{ backround: theme.brPrimary, color: theme.text }}
+          >
+            <h1 className="logo">Climate Monitor</h1>
+            <ul className="nav">
+              <li>Home</li>
+              <li>Co2</li>
+              <li>Temperature</li>
+              <li>About</li>
             </ul>
-        </div>
-    )
+          </nav>
+        )
+      }}
+    </ThemeContex.Consumer>
+  )
 }
 
-export default Navbar;
+export default Navbar
