a4f0f8f2c5e4e2fe7c01dad9dc27a65041a1fb8e
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-03-07 17:33:44 +0100

    first chart implemented: annual average co2 data

--- src/components/CarbonChart.js
+++ src/components/CarbonChart.js
@@ -1,12 +1,24 @@
 import React, { useContext } from "react"
 import { CarbonContext } from "../contexts/CarbonContext"
-// import { Line } from "react-chartjs-2"
+import { Line } from "react-chartjs-2"
 
 const CarbonChart = () => {
   const { labels, values } = useContext(CarbonContext).yearlyCarbon
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "atmospheric CO2: annual average",
+        data: values,
+        backgroundColor: ["rgba(0, 204, 255, 0.2)"]
+      }
+    ]
+  }
+  const chartOptions = { maintainAspectRatio: false }
   return (
-    <div>
-      carbon chart here: year {labels[0]} value: {values[0]}
+    <div className="chart">
+      <Line data={chartData} options={chartOptions} width={100} height={400} />
+      {/* /* carbon chart here: year {labels[0]} value: {values[0]} */}
     </div>
   )
 }
